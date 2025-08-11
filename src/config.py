5ddc21eebe6dc15e2be19beea59964625918bfc0
i.vukolov <i.vukolov@vrgaz.ru>
2025-08-11 21:18:49 +0300

    added log settings

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
+import logging
 from enum import Enum
 import os
 from pathlib import Path
@@ -7,11 +8,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-class Settings(Enum):
+class Settings:
     DEBUG: bool = os.getenv("DEBUG",)
     # Telegram.
     BOT_TOKEN: str  = os.getenv("BOT_TOKEN")
-    ADMIN_IDS: list = os.getenv("ADMIN_IDS").split(",")
+    ADMINS: list = os.getenv("ADMIN").split(",")
     CHANNEL_ID: str = os.getenv("CHANNEL_ID")
     GROUP_ID: str = os.getenv("GROUP_ID")
     # Postgres.
@@ -27,4 +28,12 @@ class Settings(Enum):
     YOOKASSA_SHOP_ID: str = os.getenv("YOOKASSA_SHOP_ID")
     YOOKASSA_SECRET_KEY: str = os.getenv("YOOKASSA_SECRET_KEY")
     # Folders.
-    ROOT_DIR = Path(__file__).resolve().parent
+    ROOT_DIR = Path(__file__).resolve()
+    # Logs
+    LOG_LEVEL = logging.INFO
+    LOGS_DIR = ROOT_DIR / 'logs'
+    LOG_FILE = LOGS_DIR / 'bot.log'
+    LOG_MAX_SIZE = 5 * 1024 * 1024 # Размер лога 5 мб.
+    LOG_BACKUP_COUNT = 3 # Количество итераций лога.
+    ENCODING = "utf-8"
+
