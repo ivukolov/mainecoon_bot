60018b886ce8ff681ad7842dbe565cf1fff9aad3
i.vukolov <i.vukolov@vrgaz.ru>
2025-08-12 13:17:44 +0300

    added routs

--- app/main.py
+++ app/main.py
@@ -5,20 +5,24 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.redis import RedisStorage
 
-from config import Settings
-from custom_logger import BotLogger, TelegramHandler
+from app.config import settings
+from app.loggers import BotLogger
+from app.loggers.handlers import TelegramHandler, file_handler
+from app.handlers import routers
 
 
 async def main():
-    bot = Bot(token=Settings.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    bot = Bot(token=settings.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     _logger = BotLogger()
-    handlers = [_logger.file_handler(), TelegramHandler(bot)]
+    handlers = (
+        file_handler(),
+        TelegramHandler(bot))
     _logger.manage_handlers(handlers)
     logger = _logger.get_logger()
-    storage = RedisStorage.from_url(
-        f"redis://{Settings.REDIS_HOST}:{Settings.REDIS_PORT}"
-    )
+    storage = RedisStorage.from_url(settings.REDIS_STORAGE)
     dp = Dispatcher(storage=storage)
+    for router in routers:
+        dp.include_router(router)
     try:
         logger.info("Запуск бота")
         await dp.start_polling(bot)
