67122c75aa453a039a09788cd9d3d4d93ec726ff
i.vukolov <i.vukolov@vrgaz.ru>
2025-08-14 16:58:47 +0300

    removed class BotLogger

--- app/loggers/logger.py
+++ app/loggers/logger.py
@@ -3,27 +3,27 @@ import logging
 from logging.handlers import RotatingFileHandler
 from typing import List, Tuple
 
-from app.config import settings
+# from app.config import settings
 
-class BotLogger:
-    def __init__(
-        self,
-        name: str = __name__,
-    ):
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(settings.LOG_LEVEL)
-        self.formatter = logging.Formatter(
-            fmt=settings.LOG_FORMATTER.get('fmt'),
-            datefmt=settings.LOG_FORMATTER.get('datefmt')
-        )
-
-
-    def manage_handlers(self, handlers: Tuple):
-        """Функция добавления кастомных хэндлеров в проект."""
-        for handler in handlers:
-            handler.setFormatter(self.formatter)
-            self.logger.addHandler(handler)
-
-    def get_logger(self) -> logging.Logger:
-        """Получить настроенный логгер"""
-        return self.logger
+# class BotLogger:
+#     def __init__(
+#         self,
+#         name: str = __name__,
+#     ):
+#         self.logger = logging.getLogger(name)
+#         self.logger.setLevel(settings.LOG_LEVEL)
+#         self.formatter = logging.Formatter(
+#             fmt=settings.LOG_FORMATTER.get('fmt'),
+#             datefmt=settings.LOG_FORMATTER.get('datefmt')
+#         )
+#
+#
+#     def manage_handlers(self, handlers: Tuple):
+#         """Функция добавления кастомных хэндлеров в проект."""
+#         for handler in handlers:
+#             handler.setFormatter(self.formatter)
+#             self.logger.addHandler(handler)
+#
+#     def get_logger(self) -> logging.Logger:
+#         """Получить настроенный логгер"""
+#         return self.logger
