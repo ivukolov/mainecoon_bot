bb0b7e9aa7cdf5ed07e22c827deaf6b2c8e361e4
i.vukolov <i.vukolov@vrgaz.ru>
2025-08-14 16:58:55 +0300

    added logining

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import logging
+import logging.config
 from logging import getLogger
 
 from aiogram import Bot, Dispatcher
@@ -8,21 +9,13 @@ from aiogram.fsm.storage.redis import RedisStorage
 from aiogram.exceptions import TelegramServerError, TelegramNotFound
 
 from config import settings
-from loggers import BotLogger
-from loggers.handlers import TelegramHandler, file_handler, setup_console_handler
 from handlers import routers, command_start_router
 
+logger = getLogger(__name__)
+logging.config.dictConfig(settings.LOGGING_CONFIG)
 
 async def main():
     bot = Bot(token=settings.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    _logger = BotLogger()
-    handlers = (
-        file_handler(),
-        setup_console_handler()
-        # TelegramHandler(bot)
-    )
-    _logger.manage_handlers(handlers)
-    logger = _logger.get_logger()
     storage = RedisStorage.from_url(settings.REDIS_STORAGE)
     dp = Dispatcher(storage=storage)
     # for router in routers:
@@ -33,14 +26,13 @@ async def main():
         await dp.start_polling(bot)
     except KeyboardInterrupt:
         logger.info("Shutting down")
-    except TelegramServerError as e:
+    except TelegramServerError:
         logger.critical('telegram server error', exc_info=True)
-    except TelegramNotFound as e:
-        logger.critical(f'Не удаётся подключится к серерам telegram', exc_info=True)
+    except TelegramNotFound:
+        logger.critical(f'Не удаётся подключится к серерам telegram')
 
 if __name__ == "__main__":
     import asyncio
-    logger = getLogger(__name__)
     try:
         asyncio.run(main())
     except Exception as e:
