3ad459c89274877fbf59512d410aa0cf910eb59e
i.vukolov <i.vukolov@vrgaz.ru>
2025-08-17 20:26:18 +0300

    added fastapi

--- app/main.py
+++ app/main.py
@@ -2,30 +2,55 @@ import logging
 import logging.config
 from logging import getLogger
 
+import uvicorn
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.redis import RedisStorage
 from aiogram.exceptions import TelegramServerError, TelegramNotFound
+from fastapi import FastAPI
+from sqladmin import Admin
 
-from middlewares.db import SafeDatabaseMiddleware
+from web.admin import app as fastapi_app, UserAdmin
+from database.db import session_factory, engine
+from middlewares.db import DatabaseMiddleware
 from config import settings
 from handlers import routers, command_start_router
 
 logger = getLogger(__name__)
 logging.config.dictConfig(settings.LOGGING_CONFIG)
 
+async def run_fastapi(app: FastAPI):
+    """Запуск FastAPI сервера"""
+    config = uvicorn.Config(
+        app,
+        host="127.0.0.1",
+        port=8000,
+        log_level="info"
+    )
+    server = uvicorn.Server(config)
+    await server.serve()
+
+async def run_bot(dp: Dispatcher, bot: Bot):
+    """Запуск бота в режиме long polling"""
+    await dp.start_polling(bot)
+
 async def main():
+    app = fastapi_app
+    admin = Admin(app, engine)
+    admin.add_view(UserAdmin)
     bot = Bot(token=settings.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     storage = RedisStorage.from_url(settings.REDIS_STORAGE)
     dp = Dispatcher(storage=storage)
     # for router in routers:
     #     dp.include_router(router)
-    dp.update.middleware(SafeDatabaseMiddleware)
+    dp.update.middleware(DatabaseMiddleware(session_factory))
     dp.include_routers(routers)
     try:
+        async with asyncio.TaskGroup() as tg:
+            tg.create_task(run_fastapi(app))
+            tg.create_task(run_bot(dp, bot))
         logger.info("Запуск бота")
-        await dp.start_polling(bot)
     except KeyboardInterrupt:
         logger.info("Shutting down")
     except TelegramServerError:
