e2d60fb6c0bc8edf038e98f569d460f49c2e3224
i.vukolov <i.vukolov@vrgaz.ru>
2025-09-14 17:19:23 +0300

    edited entry

--- app/main.py
+++ app/main.py
@@ -11,35 +11,23 @@ from aiogram.exceptions import TelegramServerError, TelegramNotFound
 from fastapi import FastAPI
 from sqladmin import Admin
 
-from web.admin import app as fastapi_app, UserAdmin
-from database.db import session_factory, engine
-from middlewares import DatabaseMiddleware, BotMiddleware, TeletonClientMiddleware
+from database.db import session_factory
+from middlewares import DatabaseMiddleware, BotMiddleware, TeletonClientMiddleware, UserMiddleware
 from app.utils.parsers import TeletonClient
+from app.web.app import run_fastapi
 from config import settings
 from handlers import routers, command_start_router
 
 logger = getLogger(__name__)
 logging.config.dictConfig(settings.LOGGING_CONFIG)
 
-async def run_fastapi(app: FastAPI):
-    """Запуск FastAPI сервера"""
-    config = uvicorn.Config(
-        app,
-        host="127.0.0.1",
-        port=8000,
-        log_level="info"
-    )
-    server = uvicorn.Server(config)
-    await server.serve()
+
 
 async def run_bot(dp: Dispatcher, bot: Bot):
     """Запуск бота в режиме long polling"""
     await dp.start_polling(bot)
 
 async def main():
-    app = fastapi_app
-    admin = Admin(app, engine)
-    admin.add_view(UserAdmin)
     # Инициализация клиента
     teletone = TeletonClient()
     # Проверка сессии в случае ошибки автоизация.
@@ -47,15 +35,15 @@ async def main():
     bot = Bot(token=settings.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     storage = RedisStorage.from_url(settings.REDIS_STORAGE)
     dp = Dispatcher(storage=storage)
-    # for router in routers:
-    #     dp.include_router(router)
+    for router in routers:
+        dp.include_router(router)
     dp.update.middleware(DatabaseMiddleware(session_factory))
+    dp.update.middleware(UserMiddleware())
     dp.update.middleware(BotMiddleware(bot))
     dp.update.middleware(TeletonClientMiddleware(teletone))
-    dp.include_routers(routers)
     try:
         async with asyncio.TaskGroup() as tg:
-            tg.create_task(run_fastapi(app))
+            tg.create_task(run_fastapi())
             tg.create_task(run_bot(dp, bot))
         logger.info("Запуск бота")
     except KeyboardInterrupt:
