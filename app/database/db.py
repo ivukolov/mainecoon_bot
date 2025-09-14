139c37ba525a579f60072b4dd1f9fe5c3ce04020
i.vukolov <i.vukolov@vrgaz.ru>
2025-09-14 17:14:02 +0300

    added get_db

--- app/database/db.py
+++ app/database/db.py
@@ -11,3 +11,7 @@ AsyncSessionLocal = async_sessionmaker(
     autoflush=False
 )
 session_factory = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
