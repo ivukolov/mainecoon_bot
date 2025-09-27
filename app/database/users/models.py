11da6f51140b9cd13078a8c863bdf74c1cb2f40f
i.vukolov <i.vukolov@vrgaz.ru>
2025-09-27 19:10:19 +0300

    added user_invites and relationship

--- app/database/users/models.py
+++ app/database/users/models.py
@@ -1,18 +1,52 @@
+import typing as t
 from datetime import datetime
-from sqlalchemy import Enum, String, Unicode, BigInteger, Text
+from logging import getLogger
+
+import sqlalchemy as sa
+from aiogram import Bot
+from aiogram.enums import ChatMemberStatus
+from aiogram.types import ChatMember
+from requests import session
 from sqlalchemy.dialects.postgresql import CITEXT, TIMESTAMP
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Mapped, mapped_column, relationship, validates, backref
 from sqlalchemy.sql import expression
 
+from exceptions import ads
 from core.models import BaseModel
 from config import settings
 from database.users.roles import UserRole
+
+logger = getLogger(__name__)
+
+# Информация об отправленных реферальных ссылках.
+user_invites = sa.Table(
+    'user_invites', BaseModel.metadata,
+    sa.Column(
+        'user_invited',
+        sa.Integer,
+        sa.ForeignKey('users.id'),
+        primary_key=True,
+        comment='Приглашённый пользователь'
+    ),
+    sa.Column(
+        'user_inviting',
+        sa.Integer,
+        sa.ForeignKey('users.id'),
+        primary_key=True,
+        comment='Кто пригласил'
+    ),
+    sa.CheckConstraint('user_invited != user_inviting', name='check_different_users'),
+    sa.UniqueConstraint('user_invited', 'user_inviting')
+)
+
 
 class User(BaseModel):
     """Класс для описания модели пользователя"""
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(
-        BigInteger,
+        sa.BigInteger,
         primary_key=True,
         autoincrement=False,
         nullable=False,
@@ -23,7 +57,7 @@ class User(BaseModel):
         }
     )
     role: Mapped[UserRole] = mapped_column(
-        Enum(UserRole),
+        sa.Enum(UserRole),
         nullable=False,
         default=UserRole.USER.value,
         server_default=UserRole.USER.value,
@@ -35,32 +69,65 @@ class User(BaseModel):
         }
     )
     username: Mapped[str] = mapped_column(
-        String(settings.USERNAME_LENGTH), nullable=True, unique=False
+        sa.String(settings.USERNAME_LENGTH), nullable=True, unique=True
     )
-    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=True,)
-    password: Mapped[str] = mapped_column(String(255), nullable=True, unique=False)
+    email: Mapped[str] = mapped_column(sa.String(255), unique=True, nullable=True,)
+    password: Mapped[str] = mapped_column(sa.String(255), nullable=True, unique=False)
     first_name: Mapped[str] = mapped_column(
-        String(settings.USER_FIRST_NAME_LENGTH), nullable=True, unique=False
+        sa.String(settings.USER_FIRST_NAME_LENGTH), nullable=True, unique=False
     )
     last_name: Mapped[str] = mapped_column(
-        String(settings.USER_LAST_NAME_LENGTH), nullable=True, unique=False
+        sa.String(settings.USER_LAST_NAME_LENGTH), nullable=True, unique=False
     )
     language_code: Mapped[str] = mapped_column(
-        String(settings.LANG_CODE_LENGTH), nullable=True, unique=False
+        sa.String(settings.LANG_CODE_LENGTH), nullable=True, unique=False
     )
     info: Mapped[str] = mapped_column(
-        String(settings.USER_INFO_LENGTH), nullable=True, unique=False
+        sa.String(settings.USER_INFO_LENGTH), nullable=True, unique=False
     )
     is_active: Mapped[bool] = mapped_column(nullable=False, default=False)
 
     posts = relationship("Post", back_populates="author", cascade="all, delete-orphan")
 
+    invited_user = relationship(
+        'User',
+        secondary=user_invites,
+        primaryjoin=(id == user_invites.c.user_inviting),
+        secondaryjoin=(id == user_invites.c.user_invited),
+        backref='inviting_users',
+        lazy='selectin', # subquery
+        uselist=False,  # Теперь это одиночный объект, а не список
+    )
+
+    @property
     def is_admin(self) -> bool:
         return self.role == UserRole.ADMIN
 
     def __str__(self) -> str:
-        return f'id пользователя:{self.id}, role: {self.role}'
+        return f'<Пользователь: @{self.username} "{self.first_name} {self.last_name}">'
+
+
+    async def add_invited_user(self, session, inviting_user: 'User') -> None:
+        self.invited_user = inviting_user
+        session.add(self.invited_user)
+        await session.commit()
+
+
+
+    async def invite_user(self, referral: t.Union[str,int], session: AsyncSession) -> None:
+        inviting_user: User = await User.one_or_none(session=session, id=referral)
+        if not inviting_user:
+            raise ads.UserNotFoundError()
+        await self.add_invited_user(session, inviting_user)
+        try:
+            await self.add_invited_user(session, inviting_user)
+        except Exception as e:
+            print(e)
+
+
 
 class TelegramSession(BaseModel):
-    name: Mapped[str] = mapped_column(Text, nullable=False, unique=True, comment='Сообщение из поста')
-    hash: Mapped[str] = mapped_column(Text, nullable=False, unique=True, comment='Хэш сессии')
+    __tablename__ = "sessions"
+
+    name: Mapped[str] = mapped_column(sa.Text, nullable=False, unique=True, comment='Сообщение из поста')
+    hash: Mapped[str] = mapped_column(sa.Text, nullable=False, unique=True, comment='Хэш сессии')
