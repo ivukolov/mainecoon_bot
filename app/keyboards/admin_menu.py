47eb9f5ff33d4491103640cdfcf520f6b7314220
i.vukolov <i.vukolov@vrgaz.ru>
2025-09-27 19:06:14 +0300

    added ADD_NEW_POSTS,UPDATE_USERS

--- app/keyboards/admin_menu.py
+++ app/keyboards/admin_menu.py
@@ -8,6 +8,8 @@ def admin_tools_menu_kb() -> ReplyKeyboardMarkup:
     buttons = [
         [KeyboardButton(text=AdminMenu.CHEK_POSTS)],
         [KeyboardButton(text=AdminMenu.PARSE_POSTS)],
+        [KeyboardButton(text=AdminMenu.ADD_NEW_POSTS)],
         [KeyboardButton(text=ActionButtons.MAIN_MENU)],
+        [KeyboardButton(text=AdminMenu.UPDATE_USERS)],
     ]
     return ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
