731eefbdf2040a568eadc04ce65528097712ee9c
i.vukolov <i.vukolov@vrgaz.ru>
2025-09-27 19:04:57 +0300

    added users_router, channel_listener, admin_router

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,8 +1,20 @@
-from handlers.start import command_start_router
+from handlers.commands import commands_router
+from handlers.main_menu import main_menu_router
 from handlers.admin import admin_router
+from handlers.blog import blog_router
+from handlers.partners import partners_router
+from handlers.ads import ads_router
+from handlers.channel_posts import channel_listener
+from handlers.users import users_router
 
 
 routers = (
-    command_start_router,
-    admin_router
+    users_router,
+    channel_listener,
+    admin_router,
+    commands_router,
+    main_menu_router,
+    blog_router,
+    partners_router,
+    ads_router,
 )
