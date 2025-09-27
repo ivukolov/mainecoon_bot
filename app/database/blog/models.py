91480c745bd2fd661d3798c4c7fc9fbbc7fd33ab
i.vukolov <i.vukolov@vrgaz.ru>
2025-09-27 19:10:00 +0300

    pep fix

--- app/database/blog/models.py
+++ app/database/blog/models.py
@@ -2,9 +2,8 @@ from datetime import datetime
 from enum import StrEnum
 from typing import Optional, List
 
-from sqlalchemy import String, Unicode, Integer, DateTime, Text, LargeBinary, BigInteger, Boolean, Float, Table, Column, \
-    ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
 
 from core.models import BaseModel
 from database.users.models import User
@@ -17,18 +16,32 @@ class MediaType(StrEnum):
     DOCUMENT = "document"
 
 
-post_tag_association = Table(
-    'post_tag_association',
-    BaseModel.metadata,
-    Column('post_id', Integer, ForeignKey('posts.id', ondelete='CASCADE'), primary_key=True),
-    Column('tag_id', Integer, ForeignKey('tags.id', ondelete='CASCADE'), primary_key=True),
+# class PostTag(BaseModel):
+#     __tablename__ = 'post_tags'
+#
+#     post_id = Column(Integer, ForeignKey('posts.id'), primary_key=True)
+#     tag_id = Column(Integer, ForeignKey('tags.id'), primary_key=True)
+#
+#     # Опциональные связи для удобства
+#     post = relationship('Post', back_populates='tags')
+#     tag = relationship('Tag', back_populates='posts')
+post_tags = sa.Table(
+    'post_tags', BaseModel.metadata,
+    sa.Column('post_id', sa.Integer, sa.ForeignKey('posts.id'), primary_key=True),
+    sa.Column('tag_id', sa.Integer, sa.ForeignKey('tags.id'), primary_key=True),
 )
+
+# user_invites = sa.Table(
+#     'user_invites', BaseModel.metadata,
+#     sa.Column('user_invited', sa.Integer, sa.ForeignKey('users.id'), primary_key=True),
+#     sa.Column('user_inviting', sa.Integer, sa.ForeignKey('users.id'), primary_key=True),
+# )
 
 class Tag(BaseModel):
     __tablename__ = "tags"
 
-    name: Mapped[str] = mapped_column(
-        String(settings.TAG_NAME_LENGTH),
+    name: orm.Mapped[str] = orm.mapped_column(
+        sa.String(settings.TAG_NAME_LENGTH),
         nullable=False,
         unique=True,
         info={
@@ -36,8 +49,8 @@ class Tag(BaseModel):
         "help_text": "Уникальный тэг для постов"
         }
     )
-    emoji: Mapped[str] = mapped_column(
-        Unicode(settings.TAG_EMOJI_LENGTH),
+    emoji: orm.Mapped[str] = orm.mapped_column(
+        sa.Unicode(settings.TAG_EMOJI_LENGTH),
         nullable=True,
         unique=False,
         info={
@@ -45,21 +58,34 @@ class Tag(BaseModel):
             "help_text": "Emoji в Unicode для добавления к тэгам"
         }
     )
-    posts = relationship("Post", secondary=post_tag_association, back_populates="tags")
+    posts: orm.Mapped[List["Post"]] = orm.relationship(
+        "Post",
+        secondary=post_tags,
+        back_populates="tags",
+        lazy="selectin"
+    )
+
 
     def __str__(self):
         return self.name
+
+    @classmethod
+    async def get_tag(cls,session,  tag_name) -> List['Post',]:
+        query = sa.select(cls).filter(cls.name.like(f"%{tag_name}%"))
+        rsult =  await session.execute(query)
+        return rsult.scalars().first()
+
 
 
 class Category(BaseModel):
     __tablename__ = "categories"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(50), unique=True)
-    slug: Mapped[str] = mapped_column(String(50), unique=True)
+    id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
+    name: orm.Mapped[str] = orm.mapped_column(sa.String(50), unique=True)
+    slug: orm.Mapped[str] = orm.mapped_column(sa.String(50), unique=True)
 
     # Отношение один-ко-многим к постам
-    posts: Mapped[List["Post"]] = relationship(
+    posts: orm.Mapped[List["Post"]] = orm.relationship(
         "Post",
         back_populates="category",
         lazy="selectin"
@@ -71,31 +97,40 @@ class Category(BaseModel):
 class Post(BaseModel):
     __tablename__ = "posts"
 
-    id: Mapped[int] = mapped_column(
-        Integer, primary_key=True, nullable=False, comment='id поста из telegram'
+    id: orm.Mapped[int] = orm.mapped_column(
+        sa.Integer, primary_key=True, nullable=False, comment='id поста из telegram', unique=True
     )
-    date: Mapped[datetime] = mapped_column(DateTime(timezone=True), comment='Дата создания поста.')
-    edit_dat: Mapped[datetime] = mapped_column(DateTime, nullable=True, comment='Дата Изменения поста.')
-    title: Mapped[str] = mapped_column(String(200), nullable=False, comment='Название поста.' )
-    message: Mapped[str] = mapped_column(Text, nullable=False, comment='Сообщение из поста')
-    author_id: Mapped[int] = mapped_column(ForeignKey("users.id", comment='Автор поста'))
-    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), comment='Категория поста')
-    # photos: Mapped[str] = mapped_column(Text, nullable=True, comment='Фото из поста')
+    date: orm.Mapped[datetime] = orm.mapped_column(sa.DateTime(timezone=True), comment='Дата создания поста.')
+    edit_date: orm.Mapped[datetime] = orm.mapped_column(sa.DateTime, nullable=True, comment='Дата Изменения поста.')
+    title: orm.Mapped[str] = orm.mapped_column(sa.String(settings.POST_TITLE_LENGTH), nullable=False, comment='Название поста.' )
+    message: orm.Mapped[str] = orm.mapped_column(sa.Text, nullable=False, comment='Сообщение из поста')
+    author_id: orm.Mapped[int] = orm.mapped_column(sa.ForeignKey("users.id", comment='Автор поста'), nullable=True)
+    category_id: orm.Mapped[int] = orm.mapped_column(sa.ForeignKey("categories.id"), comment='Категория поста', nullable=True)
+    views: orm.Mapped[int] = orm.mapped_column(sa.Integer, nullable=True, comment="Количество просмотров")
+    forwards: orm.Mapped[int] = orm.mapped_column(sa.Integer, nullable=True, comment="Количество пересылок")
+    # media: Mapped[str] = mapped_column(Text, nullable=True, comment='Фото из поста')
 
     # Один к одному.
-    author: Mapped[User] = relationship("User", back_populates="posts")
-    category: Mapped[Category] = relationship("Category", back_populates="posts")
+    author: orm.Mapped[User] = orm.relationship("User", back_populates="posts")
+    category: orm.Mapped[Category] = orm.relationship("Category", back_populates="posts")
 
-    # многие ко многим.
-    tags: Mapped[List['Tag']] = relationship(
-        secondary=post_tag_association,
-        back_populates='posts',
-        cascade='all, delete',
-        lazy='selectin',
+    tags: orm.Mapped[List["Tag"]] = orm.relationship(
+        "Tag",
+        secondary=post_tags,
+        back_populates="posts",
+        lazy = "selectin"
     )
 
     def __str__(self):
         return self.title
+
+    @classmethod
+    async def get_last_post(cls, session) -> 'Post':
+        query = sa.select(cls).order_by(cls.date.desc()).limit(1)
+        result = await session.execute(query)
+        return result.scalar_one_or_none()
+
+
 
 
 # class MediaBase:
