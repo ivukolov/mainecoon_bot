2a6cd2a4237aeb2657b50c7b1c08bf3cdec35d21
i.vukolov <i.vukolov@vrgaz.ru>
2025-09-29 17:06:46 +0300

    added MessagesService

--- app/handlers/admin.py
+++ app/handlers/admin.py
@@ -1,6 +1,5 @@
-from io import BytesIO
+import typing as t
 from logging import getLogger
-from collections import defaultdict
 
 from telethon import TelegramClient
 from telethon import types
@@ -16,8 +15,11 @@ from database import Post
 from keyboards.admin_menu import admin_tools_menu_kb
 from keyboards.lexicon import MainMenu, KeyboardBlog, AdminMenu
 from database.users.models import User
-from utils.parsers import get_media_form_message,  MessagesParser
+from utils.parsers import get_media_form_message
 from utils.decorators import admin_required
+from mappers.telegram import TelegramMessageMapper
+from mappers.schemas import TelegramMessagesListDTO
+from app.services.messages import MessagesService
 from config import settings
 
 admin_router = Router()
@@ -50,8 +52,18 @@ async def admin_menu_parse_posts(message: Message, db: AsyncSession, teleton_cli
         channel,
         limit=None,
     )]
-    parser = MessagesParser(parsed_messages, db)
-    await parser.parse_messages()
+    service = MessagesService(session=db, messages=parsed_messages, is_aiogram=False)
+    await service.service_and_save_messages()
+    # if dto_list.error_count:
+    #     not_added_messages = str(dto_list.get_error_messages_id())[:40] # Добавить корректное отоброжение
+    #     return await message.answer(
+    #         f"Не удалось акутализировать посты: {not_added_messages}",
+    #         reply_markup=admin_tools_menu_kb()
+    #     )
+    # for parsed_message in parsed_messages:
+    #     # print(parsed_message)
+    #     data = TelegramMessageMapper.from_telethon_message(parsed_message)
+    #     # print(data)
     return await message.answer(f"Все посты актуализированные!", reply_markup=admin_tools_menu_kb())
 
 @admin_router.message(F.text == AdminMenu.ADD_NEW_POSTS)
@@ -66,8 +78,6 @@ async def admin_menu_add_new_posts(message: Message,db: AsyncSession, teleton_cl
         limit=None,
         min_id=last_obj.id + 1
     )]
-    parser = MessagesParser(parsed_messages, db)
-    await parser.parse_messages()
     return await message.answer(f"Добавил новые посты босс!", reply_markup=admin_tools_menu_kb())
 
 
