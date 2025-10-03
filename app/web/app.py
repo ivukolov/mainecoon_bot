a9db800033879a766437aecd08252f4c8a805fdb
i.vukolov <i.vukolov@vrgaz.ru>
2025-10-03 23:19:26 +0300

    removed singleton added app

--- app/web/app.py
+++ app/web/app.py
@@ -1,52 +1,35 @@
 from contextlib import asynccontextmanager
 
-import uvicorn
+
 from fastapi import FastAPI, Request, APIRouter
 from fastapi.responses import RedirectResponse
 from sqladmin import Admin
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 
 from config import settings
 from core.models import BaseModel
+from database.db import engine
 from web.admin import setup_admin_panel
+from web.routes import router
+from web.midleware import DatabaseMiddleware
 
-
-class AppSingleton:
-    _instance = None
-    _app = None
-
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(AppSingleton, cls).__new__(cls)
-            cls._instance._initialize()
-        return cls._instance
-
-    def _initialize(self):
-        if self._app is None:
-            self._app = self._make_app()
-
-    def _make_app(self) -> FastAPI:
-        return FastAPI(
-            title=settings.PROJECT_NAME,
-            debug=settings.DEBUG
-        )
-
-    @property
-    def app(self) -> FastAPI:
-        """Получить экземпляр приложения"""
-        if self._app is None:
-            raise RuntimeError("Приложение не инициализировано")
-        return self._app
-
-
-app_singleton = AppSingleton()
-
-def get_app() -> FastAPI:
-    return app_singleton.app
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    debug=settings.DEBUG,
+    engine=engine,
+    middleware=[
+        Middleware(DatabaseMiddleware),
+        Middleware(
+            CORSMiddleware, allow_origins=["http://localhost", "http://127.0.0.1"]
+        ),
+    ]
+)
+setup_admin_panel(app)
+app.include_router(router)
 
 async def run_fastapi():
-    """Запуск FastAPI сервера"""
-    app = get_app()
-    setup_admin_panel(app)
+    import uvicorn
     config = uvicorn.Config(
         app,
         host="127.0.0.1",
