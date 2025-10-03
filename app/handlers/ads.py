54bac229332ecab00d02ce537739d909c8da3c1a
i.vukolov <i.vukolov@vrgaz.ru>
2025-10-03 23:18:36 +0300

    added self request check

--- app/handlers/ads.py
+++ app/handlers/ads.py
@@ -1,18 +1,15 @@
-from datetime import datetime, timedelta
 from logging import getLogger
 
 import sqlalchemy as sa
-from telethon import TelegramClient
 from aiogram import Router, F, Bot
 from aiogram.types import Message, CallbackQuery
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from config import settings
 from keyboards.lexicon import MainMenu
-from app.keyboards import ads
+from keyboards import ads
 from database import User
-from utils.bot_utils import get_referral, check_user_subscribe
-from app.exceptions.ads import DoubleSubscriptionError
+from utils.bot_utils import get_referral, check_user_subscribe, get_group_login
 
 logger = getLogger(__name__)
 
@@ -20,9 +17,13 @@ ads_router = Router()
 
 @ads_router.callback_query(ads.ReferralCheck.filter())
 async def handle_blog_btn(callback_query: CallbackQuery, callback_data: ads.ReferralCheck, db: AsyncSession, tg_user: User):
+    """Функция обработки реферальной ссылки"""
     bot = callback_query.message.bot
+    if tg_user.id == callback_data.user_id:
+        return await callback_query.message.answer('Нельзя пригласить самого себя')
+    group = get_group_login()
     is_subscribe = await check_user_subscribe(bot=bot, user_id=tg_user.id)
-    invite_link = await bot.create_chat_invite_link(settings.CHANNEL_ID)
+    #invite_link = await bot.create_chat_invite_link(settings.CHANNEL_ID)
     if is_subscribe:
         try:
             await tg_user.invite_user(referral=callback_data.referral, session=db)
@@ -30,12 +31,13 @@ async def handle_blog_btn(callback_query: CallbackQuery, callback_data: ads.Refe
             return await callback_query.message.answer('Нельзя принять инвайт дважды')
         except Exception:
             return await callback_query.message.answer('Произошла неизвестная ошибка, обратитесь к администратору канала')
-        return await callback_query.message.answer(f"Добро пожаловать в наше сообщество!")
-    return await callback_query.message.answer(f"Вы не подписаны на группу {invite_link.invite_link}")
+        return await callback_query.message.answer(f"Добро пожаловать в наше сообщество! {group}")
+    return await callback_query.message.answer(f"Вы не подписаны на группу {group}")
 
 
 @ads_router.message(F.text == MainMenu.ADS.value.name)
 async def blog_menu(message: Message, db: AsyncSession, tg_user: User):
     bot_about = await message.bot.get_me()
-    referral = await get_referral(user_id=tg_user.id, bot_name=bot_about.username)
-    await message.answer(f"Ваша ссылка на приглашение {referral}")
+    group = get_group_login()
+    referral = get_referral(user_id=tg_user.id, bot_name=bot_about.username)
+    await message.answer(f"Ваша реферальная ссылка {referral}")
