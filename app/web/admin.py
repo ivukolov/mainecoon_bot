c532e0f52391e5e1ade81ab15cdc524e1088f183
i.vukolov <i.vukolov@vrgaz.ru>
2025-10-03 23:18:07 +0300

    added authentication_backend

--- app/web/admin.py
+++ app/web/admin.py
@@ -1,17 +1,16 @@
-from fastapi.responses import RedirectResponse
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqladmin import ModelView, Admin
+from sqladmin import Admin
 
-from database.db import get_db_session, engine
+from database.db import engine
 from database.users.models import User
-from app.web.views import UserAdmin, TagAdmin, CategoryAdmin, PostAdmin
+from web.authentication import AdminAuth
+from web.views import UserAdmin, TagAdmin, CategoryAdmin, PostAdmin
 
 def setup_admin_panel(app):
+    authentication_backend = AdminAuth(secret_key="your-secret-key-here")
     admin = Admin(
         app=app,  # Will be set in main.py
         engine=engine,
-        # authentication_backend=authentication_backend,
+        authentication_backend=authentication_backend,
         title="Blog Admin",
         base_url="/admin",
         # templates_dir="templates/admin",
