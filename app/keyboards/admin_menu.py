913658f779c030cf36c57f64f1c26ba9235a9210
I.Vukolov <bloodnes2011@gmail.com>
2025-10-08 17:10:06 +0300

    added maike_interactives_kb()

--- app/keyboards/admin_menu.py
+++ app/keyboards/admin_menu.py
@@ -1,15 +1,22 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-from keyboards.lexicon import AdminMenu, ActionButtons
+from keyboards.lexicon import AdminMenu, ActionButtons, AdminInteractives
 
 
 def admin_tools_menu_kb() -> ReplyKeyboardMarkup:
     """Панель инструемнов администратора"""
     buttons = [
-        [KeyboardButton(text=AdminMenu.CHEK_POSTS)],
-        [KeyboardButton(text=AdminMenu.PARSE_POSTS)],
-        [KeyboardButton(text=AdminMenu.ADD_NEW_POSTS)],
-        [KeyboardButton(text=ActionButtons.MAIN_MENU)],
-        [KeyboardButton(text=AdminMenu.UPDATE_USERS)],
+        [KeyboardButton(text=admin_btn.value.name)] for admin_btn in AdminMenu
     ]
+    buttons.append([KeyboardButton(text=ActionButtons.MAIN_MENU)])
     return ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
+
+def maike_interactives_kb() -> InlineKeyboardMarkup:
+    btn = [
+        [
+            InlineKeyboardButton(
+                text=btn.value.name, callback_data=btn.value.callback)
+        ]
+        for btn in AdminInteractives
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=btn)
