f4d9aa05b0bc294187fe0e3d97be3e58c7505462
i.vukolov <i.vukolov@vrgaz.ru>
2025-10-10 23:15:48 +0300

    added ColumnLabelsGeneratorMixin

--- app/web/views.py
+++ app/web/views.py
@@ -1,39 +1,112 @@
 from sqladmin import ModelView
 
-from database import User, Post, Category, Tag
+from database import User, Post, Category, Tag, TelegramSession
+from sqlalchemy import inspect
 from sqlalchemy.orm import declared_attr
 
+class ReadOnlyMixin:
+    form_readonly_columns = ["created_at", "updated_at"]
 
 class PostExcludeMixin:
-    form_excluded_columns = ['posts']
+    form_excluded_columns = ['posts', "created_at", "updated_at"]
+
+class ColumnLabelsGeneratorMixin:
+    @property
+    def column_labels(self):
+        """Автоматически генерирует labels для SQLAlchemy моделей"""
+        labels = {}
+        inspector = inspect(self.model)
+
+        for column in inspector.columns:
+            # Используем comment из колонки или генерируем из имени
+            if hasattr(column, 'comment') and column.comment:
+                labels[column.name] = column.comment
+            else:
+                labels[column.name] = self._format_column_name(column.name)
+
+        return labels
+
+    def _format_column_name(self, name: str) -> str:
+        """Форматирует имя колонки в читаемый вид"""
+        # Заменяем подчеркивания на пробелы и делаем заглавные буквы
+        return name.replace('_', ' ').title()
 
 
-class UserAdmin(PostExcludeMixin, ModelView, model=User):
+class UserAdmin(ColumnlabelsGeneratorMixin, PostExcludeMixin, ReadOnlyMixin, ModelView, model=User):
     name = "Пользователь"
     name_plural = "Пользователи"
     column_list = ['id', 'username', 'is_active', 'first_name', 'last_name', 'role', 'info', 'invited_users_count']
-    form_columns = ['id', 'username', 'is_active', 'first_name', 'last_name', 'role', 'info']
+    form_columns = ['id', 'username', 'password', 'is_active', 'first_name', 'last_name', 'role', 'info']
     can_export = True
     column_sortable_list = ["id", 'role', 'is_active']
     column_default_sort = [('role', True), ('id', True)]
     column_searchable_list = ["id", "username", "first_name", "last_name"]
+    # column_labels = {
+    #     "password": "Пароль"
+    # }
+    field_labels = {
+        "username": "Имя пользователя"
+    }
+    column_formatters = {
+        "is_active": lambda model, attr: "✅ Состоит в ТГ группе" if model.is_active else "❌ Вышел из ТГ группы",
+    }
+    form_groups = {
+        "main_info": {
+            "fields": ["username", "name", "is_active"],
+            "label": "Основная информация"
+        },
+        "additional_info": {
+            "fields": ["role", "info"],
+            "label": "Дополнительная информация"
+        },
+        "system_info": {
+            "fields": ["created_at"],
+            "label": "Системная информация"
+        }
+    }
 
+    # form_widget_args = {
+    #     "password": {
+    #         "type": "password"
+    #     }
+    # }
+    async def on_model_change(self, data, model, is_created, request) -> None:
+        if is_created:
+            plain_password = data["hashed_password"]
+            data["hashed_password"] = model.get_password_hash(plain_password)
+        # Вызываем родительский метод для сохранения
+        await super().on_model_change(data, model, is_created, request)
 
-class CategoryAdmin(PostExcludeMixin, ModelView, model=Category):
+class CategoryAdmin(PostExcludeMixin, ReadOnlyMixin, ModelView, model=Category):
     name = "Категория"
     name_plural = "Категории"
     column_list = ['id', 'name', 'slug']
     can_export = True
 
 
-class TagAdmin(PostExcludeMixin, ModelView, model=Tag):
+class TagAdmin(PostExcludeMixin, ReadOnlyMixin, ModelView, model=Tag):
     name = "Тэг"
     name_plural = "Тэги"
     column_list = ['id', 'name']
     can_export = True
 
+class TelegramSessionAdmin(ReadOnlyMixin, ModelView, model=TelegramSession):
+    name = "Сессия"
+    name_plural = "Сессии"
+    column_list = ['id', 'name']
+    can_export = True
 
-class PostAdmin(ModelView, model=Post):
+    form_widget_args = {
+    "hash": {
+        "type": "textarea",
+        "rows": 10,
+        "cols": 80
+        },
+    }
+
+
+
+class PostAdmin(ColumnlabelsGeneratorMixin, ReadOnlyMixin, ModelView, model=Post):
     name = "Пост"
     name_plural = "Посты"
     column_list = ['id', 'views', 'forwards' ,'title', 'date']
@@ -41,3 +114,21 @@ class PostAdmin(ModelView, model=Post):
     column_sortable_list = ["id", "date", 'views', 'forwards']
     column_default_sort = [('id', True), ('views', True), ('forwards', True)]
     column_searchable_list = ["id", "title"]
+
+    form_widget_args = {
+    "message": {
+        "type": "textarea",
+        "rows": 10,
+        "cols": 80
+        },
+    "views": {
+        "type": "number",         # Числовое поле
+        "min": 0,                 # Минимальное значение
+        "step": 1                 # Шаг изменения
+    },
+    "forwards": {
+        "type": "number",         # Числовое поле
+        "min": 0,                 # Минимальное значение
+        "step": 1                 # Шаг изменения
+    },
+    }
