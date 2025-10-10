e6d82e79d0ea350645360a8024b4a48d7bf5d712
i.vukolov <i.vukolov@vrgaz.ru>
2025-10-10 22:22:37 +0300

    added CustomAdmi.login

--- app/web/admin.py
+++ app/web/admin.py
@@ -1,4 +1,6 @@
 from sqladmin import Admin
+from starlette.requests import Request
+from starlette.responses import JSONResponse, RedirectResponse, Response
 
 from database.db import engine
 from database.users.models import User
@@ -8,9 +10,26 @@ from web.views import UserAdmin, TagAdmin, CategoryAdmin, PostAdmin
 from config import settings
 
 
+class CustomAdmin(Admin):
+    async def login(self, request: Request) -> Response:
+        assert self.authentication_backend is not None
+
+        context = {}
+        if request.method == "GET":
+            return await self.templates.TemplateResponse(request, "sqladmin/login.html")
+
+        ok, context = await self.authentication_backend.login(request)
+        if not ok:
+            return await self.templates.TemplateResponse(
+                request, "sqladmin/login.html", context, status_code=400
+            )
+
+        return RedirectResponse(request.url_for("admin:index"), status_code=302)
+
+
 def setup_admin_panel(app):
     authentication_backend = AdminAuth(secret_key=settings.FAST_API_SECRET_KEY)
-    admin = Admin(
+    admin = CustomAdmin(
         app=app,
         engine=engine,
         authentication_backend=authentication_backend,
