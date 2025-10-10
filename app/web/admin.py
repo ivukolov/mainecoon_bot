545134f14682d8d098d294975f4d313a6a863734
i.vukolov <i.vukolov@Vrgaz.ru>
2025-10-10 09:10:08 +0300

    added settings.FAST_API_SECRET_KEY, settings.PROJECT_NAME

--- app/web/admin.py
+++ app/web/admin.py
@@ -5,13 +5,16 @@ from database.users.models import User
 from web.authentication import AdminAuth
 from web.views import UserAdmin, TagAdmin, CategoryAdmin, PostAdmin
 
+from config import settings
+
+
 def setup_admin_panel(app):
-    authentication_backend = AdminAuth(secret_key="your-secret-key-here")
+    authentication_backend = AdminAuth(secret_key=settings.FAST_API_SECRET_KEY)
     admin = Admin(
         app=app,  # Will be set in main.py
         engine=engine,
         authentication_backend=authentication_backend,
-        title="Blog Admin",
+        title=settings.PROJECT_NAME,
         base_url="/admin",
         # templates_dir="templates/admin",
     )
