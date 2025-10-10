f33816034610092f53474fb20ae870a8b1d1ce6c
i.vukolov <i.vukolov@vrgaz.ru>
2025-10-10 23:53:59 +0300

    added get_role_name

--- app/database/users/roles.py
+++ app/database/users/roles.py
@@ -7,3 +7,12 @@ class UserRole(Enum):
     USER = "USER"
     GUEST = "GUEST"
     BOT = "BOT"
+
+    @classmethod
+    def get_role_name(cls, role) -> str:
+        messages = {
+            UserRole.ADMIN: "🛠️ Администратор",
+            UserRole.USER: "👤 Пользователь",
+            UserRole.BOT: "🤖 Бот",
+        }
+        return messages.get(role)
