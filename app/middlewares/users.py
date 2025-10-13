07df6d058d6437d655f8b8cb17e11accde74ca45
I.Vukolov <bloodnes2011@gmail.com>
2025-10-13 14:53:11 +0300

    added generate_username_from_id

--- app/middlewares/users.py
+++ app/middlewares/users.py
@@ -8,6 +8,7 @@ if TYPE_CHECKING:
     from aiogram.types import TelegramObject, Message, User as TgUser
     from sqlalchemy.ext.asyncio import AsyncSession
 
+from utils.identifiers import generate_username_from_id
 from database.users.models import User
 from database.users.roles import UserRole
 from config import settings
@@ -23,11 +24,12 @@ class UserMiddleware(BaseMiddleware):
         telegram_user: TgUser = data.get("event_from_user")
         try:
             if telegram_user:
+                username = telegram_user.username or generate_username_from_id(tg_user_id=telegram_user.id)
                 user, created = await User.get_or_create(session, id=telegram_user.id, defaults={
                     'first_name': telegram_user.first_name,
                     'last_name': telegram_user.last_name,
                     'role': UserRole.BOT if telegram_user.is_bot else UserRole.USER,
-                    'username': telegram_user.username,
+                    'username': username,
                 })
                 data["tg_user"] = user
         except Exception:
