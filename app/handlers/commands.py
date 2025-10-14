30d930320fc852e7180668227e16c0bd0f09c120
I.Vukolov <bloodnes2011@gmail.com>
2025-10-14 17:23:42 +0300

    added cancel_from_any_state

--- app/handlers/commands.py
+++ app/handlers/commands.py
@@ -2,7 +2,8 @@ from datetime import datetime, timedelta
 from logging import getLogger
 import typing as t
 
-from aiogram import Router
+from aiogram import Router, F
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from aiogram.filters import CommandStart, CommandObject
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -12,7 +13,8 @@ import sqlalchemy.exc as sa_exc
 from config import settings
 from database.users.models import User
 from keyboards.main_menu import  main_menu_kb, admin_mine_menu_kb
-from keyboards.ads import referral_check_kb
+from keyboards.lexicon import ActionButtons
+from handlers.ads.keyboards import referral_check_kb
 from utils.bot_utils import check_referral
 from exceptions import ads
 
@@ -45,5 +47,16 @@ async def cmd_start(message: Message, tg_user: User, command: CommandObject, db:
         return await message.answer("Привет Босс !", reply_markup=admin_mine_menu_kb())
     return await message.answer(
         f"Привет {username}! Я — бот канала «Мейн-куны в Воронеже».\n\n",
+        reply_markup=main_menu_kb()
+    )
+
+
+@commands_router.message(F.text == ActionButtons.CANCEL.value.name)
+async def cancel_from_any_state(message: Message, tg_user: User, state: FSMContext):
+    await state.clear()
+    if tg_user and tg_user.is_admin:
+        return await message.answer("Привет Босс !", reply_markup=admin_mine_menu_kb())
+    await message.answer(
+        "✅ Возвращяемся в главное меню",
         reply_markup=main_menu_kb()
     )
