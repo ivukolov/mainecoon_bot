9920e778a30b5b5031bb31a05a652b3ef55082b0
I.Vukolov <bloodnes2011@gmail.com>
2025-10-16 17:27:05 +0300

    added PhotoSchema

--- app/schemas/ads.py
+++ app/schemas/ads.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel, Field, field_validator, ValidationError, model_validator
+from logging import getLogger
+
+from pydantic import BaseModel, Field, field_validator, ValidationError, model_validator, ConfigDict, field_serializer
 from typing import Optional, List, Union
 from datetime import date, datetime
 import re
@@ -7,27 +9,73 @@ from config import settings
 from database import User
 from keyboards.lexicon import CatGenders
 
+logger = getLogger(__name__)
+
+class PhotoSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int | None = None
+    photo_id: str | None = None
+    sort_order: int = 0
+    is_primary: bool = Field(default=False)
+
+
+
 
 class BaseSchema(BaseModel):
     """"Базовая модель для всех типов рекламных акций"""
     bot_message_title: str | None = None
     is_publicated: bool = False
     is_moderated: bool = False
-    photos: List[str] = []
+    author_id: int | None = None
+    photos: List[PhotoSchema] = Field(default_factory=list)
 
     class Config:
         from_attributes = True
 
 
+    def add_photos(self, values: list) -> 'BaseSchema':
+        for key, value in enumerate(values):
+            self.photos.append(
+                PhotoSchema(
+                    photo_id=value,
+                    sort_order=key,
+                    is_primary = True if key == 0 else False,
+                )
+            )
+        return self
+
+    def sort_photos_with_primary_first(self, photos_list: dict):
+        """Сортировка: основное фото первое, остальные по sort_order"""
+        return sorted(
+            photos_list,
+            key=lambda x: (0 if x['is_primary'] else 1, x['sort_order'])
+        )
+
+    def get_photos(self, is_sorted=False):
+        model_dump = self.model_dump(include={'photos'})['photos']
+        if is_sorted:
+            try:
+                model_dump = sorted(model_dump,key=lambda x: (0 if x['is_primary'] else 1, x['sort_order']))
+            except KeyError as e:
+                logger.error('Ошибка сортировки фотографий %s', model_dump )
+        return tuple(self.model_dump(include={'photos'})['photos'])
+
+
 
 class CatAdsSchema(BaseSchema):
+    model_config = ConfigDict(
+        from_attributes=True,  # Разрешает загрузку из ORM объектов
+        use_enum_values=True,  # Автоматически использует .value для enum
+        arbitrary_types_allowed=True  # Разрешает произвольные типы
+    )
     name: str | None = Field(
         None,
         max_length=settings.CAT_COLOR_MAX_LENGTH
     )
 
     gender: CatGenders|  str | None = None
-    birth_date: str | date | None | date = None
+    birth_date: str | date | None = None
     color: str | None = Field(
         None,
         max_length=settings.CAT_NAME_MAX_LENGTH
@@ -45,8 +93,9 @@ class CatAdsSchema(BaseSchema):
         min_length=settings.CAT_CONTACTS_MIN_LENGTH
     )
 
-    @field_validator('gender', mode='after')
-    def validate_gender(cls, value):
+
+    @field_serializer('gender')
+    def validate_gender(self, value):
         return CatGenders.get_gender(value)
 
     @field_validator('price', mode='before')
