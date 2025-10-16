ff1d53c6975dc3478da4712d732a41b977a03fcc
I.Vukolov <bloodnes2011@gmail.com>
2025-10-16 08:28:47 +0300

    added CatAd, and edited Photo models

--- app/database/blog/models.py
+++ app/database/blog/models.py
@@ -7,18 +7,11 @@ import sqlalchemy as sa
 import sqlalchemy.orm as orm
 from sqlalchemy.dialects.postgresql import JSONB
 
+from keyboards.lexicon import CatGenders
 from core.models import BaseModel
 from config import settings
 
-# class PostTag(BaseModel):
-#     __tablename__ = 'post_tags'
-#
-#     post_id = Column(Integer, ForeignKey('posts.id'), primary_key=True)
-#     tag_id = Column(Integer, ForeignKey('tags.id'), primary_key=True)
-#
-#     # Опциональные связи для удобства
-#     post = relationship('Post', back_populates='tags')
-#     tag = relationship('Tag', back_populates='posts')
+
 post_tags = sa.Table(
     'post_tags', BaseModel.metadata,
     sa.Column('post_id', sa.Integer, sa.ForeignKey('posts.id'), primary_key=True),
@@ -139,56 +132,41 @@ class Photo(BaseModel):
     __tablename__ = "photos"
 
     id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
-    # file_name: orm.Mapped[str] = orm.mapped_column(sa.String(255))
-    # file_path: orm.Mapped[str] = orm.mapped_column(sa.String(500))
-    photo_id: orm.Mapped[int] = orm.mapped_column(default=0, comment='id фотографии в телегам')
-    ad_id: orm.Mapped[int] = orm.mapped_column(sa.ForeignKey('ads.id'))
-    ad: orm.Mapped['Ad'] = orm.relationship('Ad', back_populates='photos')
+    photo_id: orm.Mapped[str] = orm.mapped_column(sa.String(200), nullable=True, comment='id фотографии в телегам')
     sort_order: orm.Mapped[int] = orm.mapped_column(default=0)
     is_primary: orm.Mapped[bool] = orm.mapped_column(default=False)
-
-
-class AdType(BaseModel):
-    """Тип объявления"""
-    __tablename__ = "ads_type"
-    id: orm.Mapped[int] = orm.mapped_column(
-        sa.Integer, primary_key=True, nullable=False, unique=True
+    cat_ad_id: orm.Mapped[int] = orm.mapped_column(
+        sa.ForeignKey('cat_ads.id'),
+        nullable=False,
+        comment='Ссылка на объявление'
     )
-    name: orm.Mapped[str] = orm.mapped_column(
-        sa.String(100), nullable=False, comment='Тип объявления'
-    )
-    slug: orm.Mapped[str] = orm.mapped_column(sa.String(50), unique=True)
-    description: orm.Mapped[t.Optional[str]] = orm.mapped_column(sa.Text, nullable=True)
-    ads: orm.Mapped[t.List['Ad']] = orm.relationship('Ad', back_populates="ad_type")
+    cat_ad: orm.Mapped['CatAd'] = orm.relationship("CatAd", back_populates="photos")
 
 
-class Ad(BaseModel):
-    __tablename__ = "ads"
+class CatAd(BaseModel):
+    __tablename__ = "cat_ads"
 
-    id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
-    author_id: orm.Mapped[int] = orm.mapped_column(sa.ForeignKey('users.id'))
-    ad_type_id: orm.Mapped[int] = orm.mapped_column(sa.ForeignKey('ads_type.id'))
-    title: orm.Mapped[str] = orm.mapped_column(sa.String(200), comment='Текст объявления')
     bot_message_title: orm.Mapped[str] = orm.mapped_column(sa.String(200), nullable=True, comment='Сообщения от бота')
     is_publicated: orm.Mapped[bool] = orm.mapped_column(default=False, comment='Опубликовано в канале')
     is_moderated: orm.Mapped[bool] = orm.mapped_column(default=False, comment='Одобрено модератором')
-    attributes: orm.Mapped[dict] = orm.mapped_column(
-        JSONB,
+    author_id: orm.Mapped[int] = orm.mapped_column(sa.ForeignKey('users.id'))
+    name: orm.Mapped[str] = orm.mapped_column(sa.String(200), nullable=False, comment='Имя')
+    gender: orm.Mapped[CatGenders] = orm.mapped_column(
+        sa.Enum(CatGenders),
         nullable=False,
-        default=dict,
-        comment='Текст объявления',
+        unique=False,
+        comment='Пол'
     )
     price: orm.Mapped[Decimal] = orm.mapped_column(sa.Numeric(12, 2), comment='Цена')
     contacts: orm.Mapped[str] = orm.mapped_column(sa.String(200), comment='Контактные данные')
+    birth_date: orm.Mapped[date] = orm.mapped_column(sa.Date, comment='Дата рождения')
+    color: orm.Mapped[str] = orm.mapped_column(sa.String(200), nullable=False, comment='Цвет')
+    cattery: orm.Mapped[str] = orm.mapped_column(sa.String(200), nullable=True, comment='Цвет')
     photos: orm.Mapped[list['Photo']] = orm.relationship(
         'Photo',
-        back_populates='ad',
         cascade='all, delete-orphan',  # автоматическое управление
         order_by='Photo.sort_order',
+        back_populates="cat_ad",
+        uselist=True,
     )
-    ad_type: orm.Mapped['AdType'] = orm.relationship('AdType')
-    author: orm.Mapped['User'] = orm.relationship("User", back_populates="ads")
-
-    __table_args__ = (
-        sa.Index('ix_ads_attributes_gin', 'attributes', postgresql_using='gin'),
-    )
+    author: orm.Mapped['User'] = orm.relationship("User", back_populates="cat_ads")
