22ef0e33756bf93d588a456705dc52193bc3324d
I.Vukolov <bloodnes2011@gmail.com>
2025-10-16 17:31:09 +0300

    added get_redis_client

--- app/clients/redis.py
+++ app/clients/redis.py
@@ -1,8 +1,23 @@
-import redis.asyncio as redis
+import typing as t
+
+from redis.asyncio.client import Redis
+from redis.asyncio.connection import ConnectionPool
 
 from config import settings
 
-redis_client =  redis.Redis(
+def get_redis_client(
+        url: t.Optional[str] = None,
+        connection_kwargs: t.Optional[t.Dict[str, t.Any]] = None,
+        **kwargs: t.Any
+) -> Redis:
+    if url is None:
+        url = settings.REDIS_CASE_STORAGE
+    if connection_kwargs is None:
+        connection_kwargs = {}
+    pool = ConnectionPool.from_url(url, **connection_kwargs)
+    return Redis(connection_pool=pool, **kwargs)
+
+redis_client =  Redis(
     host=settings.REDIS_HOST,
     port=settings.REDIS_PORT,
     db=0,
