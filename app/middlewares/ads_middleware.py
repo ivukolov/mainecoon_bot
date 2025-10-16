554c070e6292ae5e622f094e18ba9b697e560fc2
I.Vukolov <bloodnes2011@gmail.com>
2025-10-16 17:27:50 +0300

    added __init__ block

--- app/middlewares/ads_middleware.py
+++ app/middlewares/ads_middleware.py
@@ -4,10 +4,14 @@ from aiogram.dispatcher.middlewares.base import BaseMiddleware
 from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
 
 from services.ads import CatAdsService
+from utils.cache import RedisCache
 
 logger = logging.getLogger(__name__)
 
 class CatAdsServiceMiddleware(BaseMiddleware):
+    def __init__(self, cache_storage: RedisCache):
+        self.cache_storage = cache_storage
+
     async def __call__(
         self,
         handler,
@@ -15,5 +19,5 @@ class CatAdsServiceMiddleware(BaseMiddleware):
         data
     ):
         session = data.get('db')
-        data['cat_ads_service'] = CatAdsService(session=session)
+        data['cat_ads_service'] = CatAdsService(session=session, cache_storage=self.cache_storage)
         return await handler(event, data)
