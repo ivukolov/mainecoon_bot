1ec556fe68338b598ab02f9a4170b7ccd0373a35
I.Vukolov <bloodnes2011@gmail.com>
2025-10-17 16:41:39 +0300

    renamed generate_username_from_id to generate_uuid_from_str

--- app/utils/identifiers.py
+++ app/utils/identifiers.py
@@ -1,9 +1,9 @@
 import uuid
 
-def generate_username_from_id(tg_user_id: int) -> str:
+def generate_uuid_from_str(name: str) -> str:
     name_space = uuid.NAMESPACE_OID
     try:
-        username = str(uuid.uuid5(name_space, str(tg_user_id)))
+        username = str(uuid.uuid5(name_space, str(name)))
     except ValueError:
         raise ValueError('Ошибка преобразования строки при формирование UUID из id пользователя')
     return username
