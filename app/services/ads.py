04fb1e9f07292349356c5ac63fbc08ad6a21e475
I.Vukolov <bloodnes2011@gmail.com>
2025-10-17 16:38:44 +0300

    added get_pydentic_obj_from_fsm, save_ad_message

--- app/services/ads.py
+++ app/services/ads.py
@@ -2,7 +2,8 @@ import typing
 import typing as t
 from logging import getLogger
 
-from aiogram.types import InputMediaPhoto
+from aiogram import Bot
+from aiogram.types import InputMediaPhoto, Message, PhotoSize
 from sqlalchemy.ext.asyncio import AsyncSession
 import sqlalchemy as sa
 import sqlalchemy.orm as so
@@ -10,6 +11,7 @@ import sqlalchemy.orm as so
 from database import User
 from database.blog.models import CatAd, Photo
 from schemas.ads import CatAdsSchema
+from utils.bot_utils import bot_save_photo_from_message
 from utils.cache import RedisCache
 
 logger = getLogger(__name__)
@@ -25,26 +27,50 @@ class CatAdsService:
             self,
             session: AsyncSession,
             cache_storage: RedisCache,
+            bot: Bot
     ):
         self.session = session
         self.cache_storage = cache_storage
+        self.bot = bot
 
     async def save_ad_message(self, ad_message: dict, author: User):
-        cat_ads_schema: CatAdsSchema = self.schema(**ad_message)
-        cat_ads_dict = cat_ads_schema.model_dump(mode='python', exclude={'photos', 'photo_id'})
-        cat_ads_object = self.model(**cat_ads_dict, author=author)
-        self.session.add(cat_ads_object)
-        await self.session.flush()
+        photos: list = ad_message.pop('photos')
+        # Получаем объекты с фотографиями
+        try:
+            user_id = str(author.id)
+            saved_photos = []
+
+            for foto in photos:
+                photos_dict = await bot_save_photo_from_message(bot=self.bot, file_id=foto.get('file_id'), user_id=user_id)
+                saved_photos.append(photos_dict)
+        except Exception:
+            raise
+        else:
+            photos = saved_photos
         photo_objects = []
-        for idx, photo_id in enumerate(cat_ads_schema.photos):
-            photo = Photo(
-                photo_id=photo_id,
-                sort_order=idx,
-                is_primary=(idx == 0),
-                cat_ad_id=cat_ads_object.id
-            )
-            photo_objects.append(photo)
-        self.session.add_all(photo_objects)
+        try:
+            for value in photos:
+                photo = Photo(
+                    photo_id=value.photo_id,
+                    sort_order=value.sort_order,
+                    is_primary=value.is_primary,
+                )
+                photo_objects.append(photo)
+            self.session.add_all(photo_objects)
+            await self.session.flush()
+        except Exception as e:
+            await self.session.rollback()
+            raise e
+        # Получаем объект с рекламой
+        try:
+            cat_ads_object = self.model(**ad_message, author=author)
+            self.session.add(cat_ads_object)
+            await self.session.flush()
+            cat_ads_object.photos.append(photo_objects)
+        except Exception as e:
+            await self.session.rollback()
+            raise e
+        self.session.add(cat_ads_object)
         await self.session.flush()
         return cat_ads_object
 
@@ -64,16 +90,29 @@ class CatAdsService:
         return pending_cat_ads
 
 
-    def get_media_message_from_dict(self, ad_message: dict, is_sorted=False):
-        cat_ads_schema: CatAdsSchema = self.schema(**ad_message)
-        return self.get_media_message_from_schema(cat_ads_schema, is_sorted=is_sorted)
+    def get_pydentic_obj_from_fsm(
+            self,
+            foto_messages: t.Collection[Message] | None = None,
+            **kwargs
+    ) -> CatAdsSchema:
+        """Получаем pydentic объект из фсм словаря.
+        Arguments:
+            foto_messages: Сообщения с фотографиями
+            **kwargs: Словарь данных из FSM
+        """
+        cat_ads_schema: CatAdsSchema = self.schema(**kwargs)
+        if foto_messages:
+            photo = [{'photo_id': msg.photo[-1].file_id for msg in foto_messages}]
+            cat_ads_schema.add_photos(photo)
+        return cat_ads_schema
 
-    def get_media_message_from_schema(self, cat_ad_schema: CatAdsSchema, is_sorted=False):
+
+    def get_media_message_from_schema(self, cat_ad_schema: CatAdsSchema, is_sorted=False) -> t.List[InputMediaPhoto]:
         caption = cat_ad_schema.get_caption()
         photos_dict = cat_ad_schema.get_photos(is_sorted=is_sorted)
         return self.make_media_group(photos_dict, caption=caption)
 
-    def make_media_group(self, photos: tuple, caption: str) -> t.Tuple[InputMediaPhoto, ...]:
+    def make_media_group(self, photos: tuple, caption: str) -> t.List[InputMediaPhoto]:
         media = []
         if photos:
             for idx, photo in enumerate(photos):
@@ -84,4 +123,4 @@ class CatAdsService:
                 if idx == 0:
                     input_media.caption = caption
                 media.append(input_media)
-        return tuple(media)
+        return media
