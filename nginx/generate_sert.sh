df0221b4ec940475d89d94aba272d5bc9740639f
i.vukolov <wukolov-ilya@yandex.ru>
2025-10-29 21:46:57 +0300

    moved conf

--- nginx/generate_sert.sh
+++ nginx/generate_sert.sh
@@ -1,4 +1,4 @@
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
   -keyout ./nginx/ssl/privkey.pem \
   -out ./nginx/ssl/fullchain.pem \
-  -config ./nginx/ssl/ssl.conf
+  -config ./ssl/ssl.conf
